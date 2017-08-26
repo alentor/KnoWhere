7466acd8ab2e6faa7fd13da47e9f25ad3c627c60
Williams Guterman <alentorz@gmail.com>
2017-08-26 08:14:35 +0300

    Add settings support to api

--- KnoWhere.API/Controllers/PlacesController.cs
+++ KnoWhere.API/Controllers/PlacesController.cs
@@ -3,9 +3,11 @@ using System.Net;
 using System.Threading.Tasks;
 using Communication;
 using Communication.Response;
+using KnoWhere.API.Config;
 using KnoWhere.API.Core.PlacesJsonParser.GoogleParser;
 using KnoWhere.API.Core.PlacesJsonParser.GoogleParser.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
 namespace KnoWhere.API.Controllers
@@ -13,19 +15,28 @@ namespace KnoWhere.API.Controllers
     [Route("api/[controller]")]
     public class PlacesController : Controller
     {
+        private readonly Settings _settings;
         private readonly GoogleJsonParser _GoogleJsonParser = new GoogleJsonParser();
+
+        // Places controller constructor.
+        public PlacesController(IOptions<Settings> optionsAccessor)
+        {
+            _settings = optionsAccessor.Value;
+        }
 
         // GET api/Places
         // E.G http://localhost:10607/api/places?Language=en&location.Latitude=32.07861&location.Longitude=34.881487
         [HttpGet]
         public async Task<ContentResult> Get(PlacesRequest request)
         {
+            string asd = _settings.PlacesApiKey;
             if (!ModelState.IsValid) return Content(JsonConvert.SerializeObject(new PlacesResponse { isSucess = false }), "application/json");
             GooglePlacesResult googlePlacesResult;
-            WebRequest webRequest = WebRequest.Create($"https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyBzMmtL9lw3TVKrlkL5aVqVLw6lFy-mAcs&location={request.Location.Latitude},{request.Location.Longitude}&radius=2000");
+            WebRequest webRequest = WebRequest.Create($"https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={_settings.PlacesApiKey}&location={request.Location.Latitude},{request.Location.Longitude}&radius=2000");
             using (WebResponse webResponse = webRequest.GetResponse())
             {
-                if (webResponse.GetResponseStream() == null) return Content(JsonConvert.SerializeObject(new PlacesResponse { isSucess = false }), "application/json");
+                if (webResponse.GetResponseStream() == null)
+                    return Content(JsonConvert.SerializeObject(new PlacesResponse { isSucess = false }), "application/json");
                 using (StreamReader streamReader = new StreamReader(webResponse.GetResponseStream()))
                 {
                     string jsonResponse = streamReader.ReadToEnd();
--- KnoWhere.API/Startup.cs
+++ KnoWhere.API/Startup.cs
@@ -1,28 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using KnoWhere.API.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace KnoWhere.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        private IConfiguration Configuration { get; }
 
-        public IConfiguration Configuration { get; }
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("Config/appsettings.json", true, true);
+            Configuration = builder.Build();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adds services required for using options.
+            services.AddOptions();
+            // Register the IConfiguration instance which MyOptions binds against.
+            services.Configure<Settings>(Configuration);
+            // Add MVC service.
             services.AddMvc();
         }
 
@@ -33,7 +37,6 @@ namespace KnoWhere.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseMvc();
         }
     }
