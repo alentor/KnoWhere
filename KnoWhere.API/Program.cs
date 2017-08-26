c5dcb10c9fb412c4ad6a84ca13744ba5eb065ffb
William Alen Guterman <alentorz@gmail.com>
2017-08-26 16:38:05 +0300

    Update webapi to use Kestrel server

--- KnoWhere.API/Program.cs
+++ KnoWhere.API/Program.cs
@@ -14,7 +14,14 @@ namespace KnoWhere.API
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+            //BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
